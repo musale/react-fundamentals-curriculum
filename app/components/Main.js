d4a1071c06c734b4bf5fb6d53f2795620fe83a50
musale <martinmshale@gmail.com>
2017-01-20 13:41:04 +0300

    initial project setup

---
+++ app/components/Main.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Main = React.createClass({
+  render: function () {
+    return (
+        <div> Main Component </div>
+    )
+  }
+});
+
+module.exports = Main;
