2d279efe703cb43776df1a2872475291c426f253
musale <martinmshale@gmail.com>
2017-01-23 18:36:53 +0300

    fixed main js file

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,9 +1,16 @@
 var React = require('react');
+var styles = require('../styles/styles');
+var GetCityContainer = require('../containers/GetCityContainer');
+var Header = require('../components/Header');
+require('../main.css');
 
 var Main = React.createClass({
   render: function () {
     return (
-        <div> Main Component </div>
+        <div style={styles.main}>
+            <Header />
+            {this.props.children}
+        </div>
     )
   }
 });
