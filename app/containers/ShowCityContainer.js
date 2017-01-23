3def4f7215de2359dc188a3cab49fecfd356c3de
musale <martinmshale@gmail.com>
2017-01-23 18:35:55 +0300

    added new file ShowCityCOntainer

---
+++ app/containers/ShowCityContainer.js
@@ -0,0 +1,41 @@
+var React = require('react');
+var ShowCity = require('../components/ShowCity');
+var helpers = require('../utils/helpers');
+
+var ShowCityContainer = React.createClass({
+    getInitialState: function() {
+        return {
+            isLoading: true,
+            forecastData: {}
+        }
+    },
+    componentDidMount: function() {
+        this.makeRequest(this.props.routeParams.city)
+    },
+    componentWillReceiveProps: function(nextProps) {
+        this.makeRequest(nextProps.routeParams.city)
+    },
+    makeRequest: function(city) {
+        helpers.getForecast(city)
+            .then(function(forecastData) {
+                this.setState({
+                    isLoading: false,
+                    forecastData: forecastData
+                });
+            }.bind(this));
+    },
+    render: function() {
+        return ( <
+            ShowCity city = {
+                this.props.routeParams.city
+            }
+            isLoading = {
+                this.state.isLoading
+            }
+            forecastData = {
+                this.state.forecastData
+            }
+            />
+        )
+    }
+})
