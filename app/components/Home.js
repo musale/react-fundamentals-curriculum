44b0ca052e8e24c607701e67ad76e7bbdc84797a
musale <martinmshale@gmail.com>
2017-01-23 18:36:25 +0300

    added the home component

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,11 +1,16 @@
 var React = require('react');
+var styles = require('../styles/styles');
+var GetCityContainer = require('../containers/GetCityContainer');
 
-var Home = React.createClass({
-  render: function () {
+function Home(props){
     return (
-        <div> Home Component </div>
+        <div style={styles.home}>
+            <h1 style={styles.title}>
+                Enter a City and State
+            </h1>
+            <GetCityContainer />
+        </div>
     )
-  }
-});
+}
 
 module.exports = Home;
