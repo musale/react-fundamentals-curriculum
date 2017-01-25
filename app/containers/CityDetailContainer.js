58b7b46c89d0040e7c0c02526edbc0a206846238
musale <martinmshale@gmail.com>
2017-01-25 19:41:26 +0300

    :heavy_plus_sign: added some new project files
    new route that will display the details of that day

---
+++ app/containers/CityDetailContainer.js
@@ -0,0 +1,12 @@
+var React = require('react');
+var CityDetail = require('../components/CityDetail');
+
+var CityDetailContainer = React.createClass({
+    render: function() {
+        return (
+            <CityDetail />
+        )
+    }
+})
+
+module.exports = CityDetailContainer;
