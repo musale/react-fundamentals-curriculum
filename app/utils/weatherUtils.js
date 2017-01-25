b2c85c2d7b8ed605b3e2af6ad03a3bb2729e7a06
musale <martinmshale@gmail.com>
2017-01-25 18:27:56 +0300

    Changed the names of the months into longer names
    I have changed the names from 3-letters to the full name of the month ```i.e. Jan``` is now ```January```
    
    IMP #1

--- app/utils/weatherUtils.js
+++ app/utils/weatherUtils.js
@@ -9,18 +9,18 @@ var daysMap = {
 };
 
 var monthsMap = {
-    "0": "Jan",
-    "1": "Feb",
-    "2": "Mar",
-    "3": "Apr",
+    "0": "January",
+    "1": "February",
+    "2": "March",
+    "3": "April",
     "4": "May",
     "5": "June",
     "6": "July",
-    "7": "Aug",
-    "8": "Sept",
-    "9": "Oct",
-    "10": "Nov",
-    "11": "Dec"
+    "7": "August",
+    "8": "September",
+    "9": "October",
+    "10": "November",
+    "11": "December"
 };
 
 function convertTemp(kelvin) {
