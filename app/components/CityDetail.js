6f189ea0e19928055b69926a1c547ac3f7e04d69
musale <martinmshale@gmail.com>
2017-01-31 18:27:20 +0300

    added the city details

--- app/components/CityDetail.js
+++ app/components/CityDetail.js
@@ -1,8 +1,32 @@
 var React = require('react');
+var utils = require('../utils/weatherUtils');
+var DayItem = require('./DayItem');
+var convertTemp = utils.convertTemp;
 
-function CityDetail(props){
-    return(
-        <div>City detail</div>
+var styles = {
+    descriptionContainer: {
+        fontSize: 34,
+        fontWeight: 100,
+        maxWidth: 400,
+        margin: '0 auto',
+        textAlign: 'center'
+    }
+}
+
+function CityDetail(props) {
+    return (
+        <div>
+            <DayItem day={props.weather}/>
+            <div style={styles.descriptionContainer}>
+                <p>{props.city}</p>
+                <p>{props.weather.weather[0].description}</p>
+                <p>min temp: {convertTemp(props.weather.temp.min)}
+                    degrees</p>
+                <p>max temp: {convertTemp(props.weather.temp.max)}
+                    degrees</p>
+                <p>humidity: {props.weather.humidity}</p>
+            </div>
+        </div>
     )
 }
 
